1bca288f78a3bba92858cbcdd88ee2d04f4837d7
Patryk Tomlak <34221450+PTomalak@users.noreply.github.com>
2024-04-01 18:03:41 -0400

    update spawner

--- arch-vnc/spawn.sh
+++ arch-vnc/spawn.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 NICKNAME="cguser"
 
+chmod +x ./scripts/*
 docker stop -t 1 arch-vnc && docker rm arch-vnc
 docker build -t arch-vnc .
 docker run -d --name arch-vnc -p 5900:5900 -p 6080:6080 -e VNC_PASSWD=password -v ./../:/CS434CG -v ./persist:/home/$NICKNAME -e USERNAME=$NICKNAME arch-vnc
+echo ""
 echo "http://localhost:6080"
