5afb285cb601929294d84d3344cb481c333bac17
Patryk Tomlak <34221450+PTomalak@users.noreply.github.com>
2024-04-01 20:38:21 -0400

    backup persist

---
+++ arch-vnc/spawn_no_persist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+NICKNAME="cguser"
+
+currdir=$(pwd | awk -F/ '{print $NF}')
+
+if [ "$currdir" != "arch-vnc" ]; then
+    echo "You are not in correct folder, go to 'arch-vnc'"
+    exit 1
+fi
+
+chmod +x ./scripts/*
+docker stop -t 1 arch-vnc && docker rm arch-vnc
+docker build -t arch-vnc .
+docker run -d --name arch-vnc -p 5900:5900 -p 6080:6080 -e VNC_PASSWD=password -v ./../:/CS434CG -e USERNAME=$NICKNAME arch-vnc
+echo ""
+echo "http://localhost:6080"
